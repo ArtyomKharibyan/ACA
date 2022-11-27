5aa50f0b5e4cf1fe716866b21b48e24b4984c5ec
ArtyomKharibyan <kharibyanartyom@gmail.com>
2022-11-27 22:44:48 +0400

    Create Homework2.js

---
+++ Homework2.js
@@ -0,0 +1,10 @@
+function findMaxRec(A, n) {
+    if (n === 1) return A[0];
+  
+    return Math.max(A[n - 1], findMaxRec(A, n - 1));
+  }
+  let A = [1, 2, 3, 4];
+  let n = A.length;
+  
+  console.log(findMaxRec([1, 2, 3, 4, 5], n));
+  
