f6444a71c97944cf920bba16448661ddbc1fd2c2
ArtyomKharibyan <kharibyanartyom@gmail.com>
2022-11-28 00:14:11 +0400

    Update Homework2.js

--- Recursion-Homework/Homework2.js
+++ Recursion-Homework/Homework2.js
@@ -1,10 +1,10 @@
-function findMaxRec(A, n) {
+function findMaxNum(A, n) {
     if (n === 1) return A[0];
   
-    return Math.max(A[n - 1], findMaxRec(A, n - 1));
+    return Math.max(A[n - 1], findMaxNum(A, n - 1));
   }
   let A = [1, 2, 3, 4];
   let n = A.length;
   
-  console.log(findMaxRec([1, 2, 3, 4, 5], n));
+  console.log(findMaxNum([1, 2, 3, 4, 5], n));
   
